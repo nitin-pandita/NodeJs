477a6861a3d5a1ac92adac724ac26ffe41b92e9f
nitin-pandita <nitin_207013@saitm.org>
2023-07-31 11:59:39 +0530

    using the env port

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ app.get("/", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send([1, 3, 4]);
 });
-app.listen(3000, () => console.log("Listening on port 3000"));
+//? Setting the Environment Port
+const port = process.env.PORT || 3000;
+
+//* If the port value was giving then that will be set otherwise the hardcoded 3000 value will be used for the port
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
